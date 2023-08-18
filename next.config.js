4b91581742eb39d026a22c0a69eca95780fe0f61
Nashia <nashiadave@yahoo.com>
2023-08-18 17:45:31 +0100

    Dependencies Updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa');
 
-module.exports = nextConfig
+module.exports = withPWA({
+    reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+  },
+ 
+});
